b3aa63a991a48681d48ee1da1dec6e051f7d4008
Laotao <1057177898@qq.com>
2022-08-22 22:32:56 +0800

    push commit

--- GitDemo/src/main/java/com/aiguigu/git/GitTest.java
+++ GitDemo/src/main/java/com/aiguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git !!");
         System.out.println("hello git !!");
         System.out.println("hello git1 !!");
+        System.out.println("push git1 !!");
     }
 }
